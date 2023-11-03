37f609b44c440eb5b96b68ae54decd516e533951
Alain-Kay <kazadikayumba@gmail.com>
2023-11-03 09:59:10 +0100

    data generate

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -3,14 +3,28 @@
 namespace Database\Factories;
 
 use App\Models\Company;
-use LaravelLiberu\Companies\Database\Factories\CompanyFactory as CoreCompanyFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-class CompanyFactory extends CoreCompanyFactory
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Company::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company,
+            'address' => $this->faker->streetAddress,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'zip' => $this->faker->postcode,
+            'phone_number' => $this->faker->phoneNumber,
+            'website' => $this->faker->url,
+            'industry' => $this->faker->word,
+            'description' => $this->faker->text,
+        ];
+    }
 }
